22a773f28978afc9e8eb58e35b6351f33ac658ef
rtiare <ttorosa55@gmail.com>
2021-01-27 11:27:11 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="search.."></input>
+
+        </div>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
