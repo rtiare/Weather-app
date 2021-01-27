218257b43b3006c08099182b3853bf3e28798d37
rtiare <ttorosa55@gmail.com>
2021-01-27 13:00:57 -0800

    Added style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+//api number
+const api = {
+  key: "c3075aa0dcec8bd643bcd3307b19c1f2",
+  base: "https://api.openweathermap.org/data/2.5"
+}
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="search.."></input>
-
+          <input type="text" className="search-bar" placeholder="search.." />
         </div>
       </main>
-
     </div>
   );
 }
