58585d36d31bc31ebc65baaf861aaa9547a295b5
rtiare <ttorosa55@gmail.com>
2021-01-27 14:05:20 -0800

    added variables

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 //api number
+//base number
+//commit update
+//bunch of 
 const api = {
   key: "c3075aa0dcec8bd643bcd3307b19c1f2",
   base: "https://api.openweathermap.org/data/2.5"
 }
+
+let f = 0;
+let k = 3;
 
 function App() {
   return (
